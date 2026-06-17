15f3019e25832f82cc256420e2f1da508030c08e
Ayaz Ali <ayazali935915@gmail.com>
2026-06-17 10:43:02 +0530

    Time: 18 ms (95.40%) | Memory: 24 MB (51.72%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len = 0;
+
+        // Find final length
+        for (char c : s) {
+            if (c == '*') {
+                len = max(0LL, len - 1);
+            } 
+            else if (c == '#') {
+                len *= 2;
+            } 
+            else if (c != '%') {
+                len++;
+            }
+        }
+
+        if (k >= len) return '.';
+
+        // Work backwards
+        for (int i = s.size() - 1; i >= 0; i--) {
+            char c = s[i];
+
+            if (c == '*') {
+                len++;
+            } 
+            else if (c == '#') {
+                len /= 2;
+                if (k >= len) k -= len;
+            } 
+            else if (c == '%') {
+                k = len - 1 - k;
+            } 
+            else {
+                len--;
+                if (k == len) return c;
+            }
+        }
+
+        return '.';
+    }
+};
