d3da75450d8b6f0150171a38b3fa79bf2e2cae76
Ayaz Ali <ayazali935915@gmail.com>
2026-06-19 15:51:59 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (97.89%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -4,9 +4,8 @@ public:
         int l=0,r=arr.size()-1;
         while(l<r){
             int mid = l+(r-l)/2;
-            if(arr[mid]<arr[mid+1]){
-                l=mid+1;
-            } else r=mid;
+            if(arr[mid]<arr[mid+1]) l=mid+1;
+             else r=mid;
         }
         return l;
     }
