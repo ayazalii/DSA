4a1de507ec222b0d9866cd151b9e2bab944c526f
Ayaz Ali <ayazali935915@gmail.com>
2026-06-24 10:05:50 +0530

    Time: 570 ms (67.44%) | Memory: 74 MB (11.63%) - LeetSync

---
+++ 3964-number-of-zigzag-arrays-ii/number-of-zigzag-arrays-ii.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    static const long long MOD = 1e9 + 7;
+
+    using Matrix = vector<vector<long long>>;
+
+    Matrix multiply(Matrix &A, Matrix &B) {
+        int n = A.size();
+        Matrix C(n, vector<long long>(n, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+                if (A[i][k] == 0) continue;
+
+                for (int j = 0; j < n; j++) {
+                    if (B[k][j] == 0) continue;
+
+                    C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
+                }
+            }
+        }
+        return C;
+    }
+
+    Matrix power(Matrix base, long long exp) {
+        int n = base.size();
+
+        Matrix res(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; i++) res[i][i] = 1;
+
+        while (exp) {
+            if (exp & 1) res = multiply(res, base);
+            base = multiply(base, base);
+            exp >>= 1;
+        }
+
+        return res;
+    }
+
+    int zigZagArrays(int n, int l, int r) {
+        int m = r - l + 1;
+        int S = 2 * m;
+
+        Matrix T(S, vector<long long>(S, 0));
+
+        // up(x) -> down(y), y > x
+        for (int x = 0; x < m; x++) {
+            for (int y = x + 1; y < m; y++) {
+                T[y][m + x] = 1;
+            }
+        }
+
+        // down(x) -> up(y), y < x
+        for (int x = 0; x < m; x++) {
+            for (int y = 0; y < x; y++) {
+                T[m + y][x] = 1;
+            }
+        }
+
+        Matrix P = power(T, n - 1);
+
+        long long ans = 0;
+
+        for (int i = 0; i < S; i++) {
+            for (int j = 0; j < S; j++) {
+                ans = (ans + P[i][j]) % MOD;
+            }
+        }
+
+        return (int)ans;
+    }
+};
