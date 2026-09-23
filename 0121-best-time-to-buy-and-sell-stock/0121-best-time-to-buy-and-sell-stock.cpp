9b7e1f97716e62785f06f6f7ec77a326da601835
Ayaz Ali <ayazali935915@gmail.com>
2026-09-24 01:22:22 +0530

    Time: 0 ms (100%), Space: 97.4 MB (26.4%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+void maxProfitFinder(vector<int>& prices,int i,int&minPrice,int&maxProfit){
+    //base case
+    if(i==prices.size()) return;
+    //one case
+    if(prices[i]<minPrice) minPrice=prices[i];
+    int todaysProfit= prices[i]-minPrice;
+    if(todaysProfit>maxProfit) maxProfit=todaysProfit;
+    //Recursion time beta
+    maxProfitFinder(prices,i+1,minPrice,maxProfit);
+}
+    int maxProfit(vector<int>& prices) {
+       int minPrice=INT_MAX;
+       int maxProfit=INT_MIN;
+       maxProfitFinder(prices,0,minPrice,maxProfit);
+       return maxProfit; 
+    }
+};
